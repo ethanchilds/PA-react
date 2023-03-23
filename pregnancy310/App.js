a6d16b17e2eca179d09fb1f946f021f1e93e94d0
DaYlAmYaT <lamyatkelvin@gmail.com>
2023-03-23 11:44:19 -0600

    nested home problem fixed

--- pregnancy310/App.js
+++ pregnancy310/App.js
@@ -14,7 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="Home"
+          name="Main"
           component={MainScreen}
           options={{ headerShown: false }}
         />
