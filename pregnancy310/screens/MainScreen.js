6cb07f3614a806e1c316a5a4a7cbfe0164d12594
DaYlAmYaT <lamyatkelvin@gmail.com>
2023-03-23 12:04:59 -0600

    add icons to the tabbar

--- pregnancy310/screens/MainScreen.js
+++ pregnancy310/screens/MainScreen.js
@@ -6,16 +6,65 @@ import UserScreen from './UserScreen'
 import SettingScreen from './SettingScreen'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 function Main() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen}/>
-      <Tab.Screen name="Names" component={NamesScreen}/>
-      <Tab.Screen name="User" component={UserScreen} />
-      <Tab.Screen name="Setting" component={SettingScreen} />
+      <Tab.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <Ionicons
+              name="home-outline"
+              size={tabInfo.focused ? 26 : 20}
+              color={tabInfo.tintColor}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Names"
+        component={NamesScreen}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <Ionicons 
+              name="list-outline"
+              size={tabInfo.focused ? 26 : 20}
+              color={tabInfo.tintColor}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <Ionicons 
+              name="person-outline"
+              size={tabInfo.focused ? 26 : 20}
+              color={tabInfo.tintColor}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{
+          tabBarIcon: (tabInfo) => (
+            <Ionicons 
+              name="settings-outline"
+              size={tabInfo.focused ? 26 : 20}
+              color={tabInfo.tintColor}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
